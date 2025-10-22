b9dac724c376d7eb9819aa51f63a48417a0f8559
kentq6 <kentq6@gmail.com>
2025-10-22 17:44:14 -0400

    Add login and registration pages with Clerk authentication components

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,19 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <main className="flex flex-col items-center p-5 gap-1 animate-fade-in min-h-screen justify-center bg-white dark:bg-gray-800">
+      <Image
+        src="/F1-unofficial-logo.svg"
+        width={300}
+        height={300}
+        alt="Logo"
+      />
+
+      <div className="mt-1">
+        <SignIn />
+      </div>
+    </main>
+  );
+}
