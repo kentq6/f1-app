f97c6950458a1062fcfd68b634aa2ddf859926a8
kentq6 <kentq6@gmail.com>
2025-10-22 17:45:39 -0400

    Add link back to home page from login and register pages

--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -1,15 +1,18 @@
 import { SignUp } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function RegisterPage() {
   return (
     <main className="flex flex-col items-center p-5  gap-1 animate-fade-in min-h-screen justify-center bg-white dark:bg-gray-800">
-      <Image
-        src='/F1-unofficial-logo.svg'
-        width={300}
-        height={300}
-        alt="Logo"
-      />
+      <Link href="/">
+        <Image
+          src="/F1-unofficial-logo.svg"
+          width={300}
+          height={300}
+          alt="Logo"
+        />
+      </Link>
 
       <div className="mt-1">
         <SignUp />
