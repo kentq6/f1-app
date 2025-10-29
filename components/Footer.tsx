026d6350d3f73a78929e266eeb459cf78d88a5f6
kentq6 <kentq6@gmail.com>
2025-10-29 00:31:50 -0400

    Add margin to top of footer to create space from main page components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="relative overflow-hidden bg-white border-t border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <footer className="mt-10 relative overflow-hidden bg-white border-t border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Logo */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
