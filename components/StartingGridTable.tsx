9da3cda519fc9bdcb0a4b703a625888cfe5e68dd
kentq6 <kentq6@gmail.com>
2025-10-30 01:41:26 -0400

    Add SessionResultTable and StartingGridTable components for future implementation

---
+++ components/StartingGridTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StartingGridTable = () => {
+  return (
+    <div className="w-full h-full bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-2xl shadow-xl border border-gray-100/50 dark:border-gray-700/50 p-4">StartingGridTable</div>
+  )
+}
+
+export default StartingGridTable
