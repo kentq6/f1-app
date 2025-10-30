79b529cae9f255880a1306cf0789d4310653b47b
kentq6 <kentq6@gmail.com>
2025-10-30 03:54:44 -0400

    Enhance Loading component by integrating theme support and updating text color for better visibility

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,12 +1,17 @@
-"use client";
-
+import { useTheme } from 'next-themes';
 import React from 'react'
 import { BlinkBlur } from 'react-loading-indicators';
 
 const Loading = () => {
+  const { theme } = useTheme();
+
   return (
     <div className="flex flex-col items-center justify-center animate-fade-in pt-16">
-      <BlinkBlur color="var(--color-formula-one-primary)" size="large" text="Loading..." textColor="black" />
+      <BlinkBlur 
+        color="var(--color-formula-one-primary)"
+        size="large" text="Loading..."
+        textColor="gray"
+      />
     </div>
   );
 }
