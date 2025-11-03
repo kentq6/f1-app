494a80fe90c81fc6965758b04023c69061d7dcab
kentq6 <kentq6@gmail.com>
2025-11-03 14:55:43 -0500

    Enhance HomePage layout by adding borders to session info and weather components

--- app/page.tsx
+++ app/page.tsx
@@ -170,38 +170,38 @@ const HomePage = () => {
 
       {/* Grid Layout */}
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-        <div className="bg-primary-foreground p-4 rounded-lg">
+        <div className="bg-primary-foreground p-4 rounded-lg border border-border dark:border-border">
           <SessionInfo filteredSession={filteredSession} />
         </div>
         {/* Small Component */}
-        <div className="bg-primary-foreground p-4 rounded-lg">
+        <div className="bg-primary-foreground p-4 rounded-lg border border-border dark:border-border">
           <WeatherInfo filteredSession={filteredSession} />
         </div>
         {/* Long Component */}
         {filteredSession?.session_type === "Qualifying" ? (
-          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2 border border-border dark:border-border">
             <StartingGridTable
               filteredSession={filteredSession}
               driversData={driversData}
             />
           </div>
         ) : (
-          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2 border border-border dark:border-border">
             <SessionResultsTable
               filteredSession={filteredSession}
               driversData={driversData}
             />
           </div>
         )}
-        <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-        <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <div className="bg-primary-foreground p-4 rounded-lg border border-border dark:border-border">Test</div>
+        <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2 border border-border dark:border-border">
           <StintsChart
             key="TireStintChart"
             filteredSession={filteredSession}
             driversData={driversData}
           />
         </div>
-        <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+        <div className="bg-primary-foreground p-4 rounded-lg border border-border dark:border-border">Test</div>
       </div>
     </main>
   );
