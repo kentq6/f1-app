7041efbb733ff0abc47fea43080076c36406274b
kentq6 <kentq6@gmail.com>
2025-11-07 11:35:46 -0500

    Update sign-in and sign-up page styles to use primary foreground background for improved visual consistency across the application

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center p-5  gap-1 animate-fade-in min-h-screen justify-center bg-white dark:bg-gray-800">
+    <main className="flex flex-col items-center p-5 gap-1 animate-fade-in min-h-screen justify-center bg-primary-foreground dark:bg-primary-foreground">
       <Logo height={150} width={150} />
 
       <div className="mt-1">
