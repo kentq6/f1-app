8c2a1bf3d5195a28dbc14a09e2d04d4ee0f299db
kentq6 <kentq6@gmail.com>
2025-11-20 15:53:39 -0500

    Enhance DriverData interface to include StartingGrid for improved data structure and functionality

--- types/driverData.ts
+++ types/driverData.ts
@@ -1,6 +1,7 @@
 import { SessionResult } from "./sessionResult";
+import { StartingGrid } from "./startingGrid";
 
-export interface DriverData extends SessionResult {
+export interface DriverData extends SessionResult, StartingGrid {
   name_acronym?: string;
   team_colour?: string;
   team_name?: string;
