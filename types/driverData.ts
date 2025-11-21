ba6b1e7c17d978600094a52ee238db3244a08193
kentq6 <kentq6@gmail.com>
2025-11-21 17:22:17 -0500

    Remove DriverData interface as part of the refactor to streamline data handling and improve overall code structure

--- types/driverData.ts
+++
@@ -1,9 +0,0 @@
-import { SessionResult } from "./sessionResult";
-import { StartingGrid } from "./startingGrid";
-
-export interface DriverData extends SessionResult, StartingGrid {
-  name_acronym?: string;
-  team_colour?: string;
-  team_name?: string;
-  headshot_url?: string;
-}
