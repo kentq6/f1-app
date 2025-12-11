ec01e2476b40c7e5fc08e1648414930e3d0885c3
kentq6 <kentq6@gmail.com>
2025-12-11 01:15:32 -0500

    Rename sign-in and sign-up page components for clarity

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/components/Logo";
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
+export default function SignInPage() {
   return (
     <main className="flex flex-col items-center p-5 gap-1 animate-fade-in min-h-screen justify-center bg-primary-foreground dark:bg-primary-foreground">
       <Logo height={150} width={150} />
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/components/Logo";
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
+export default function SignUpPage() {
   return (
     <main className="flex flex-col items-center p-5 gap-1 animate-fade-in min-h-screen justify-center bg-primary-foreground dark:bg-primary-foreground">
       <Logo height={150} width={150} />
