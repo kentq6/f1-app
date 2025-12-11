4c0a172a6c9c18502406afeef9855cadcdb733c3
kentq6 <kentq6@gmail.com>
2025-12-11 01:23:36 -0500

    Update Loading component to ensure full-screen coverage with min-height and min-width styles

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ import { BlinkBlur } from "react-loading-indicators";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center animate-fade-in pt-16">
+    <div className="flex flex-col items-center justify-center animate-fade-in min-h-screen min-w-full">
       <BlinkBlur
         color="var(--color-formula-one-primary)"
         size="large"
