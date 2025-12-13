7c8025921993f6a760b2251a2fcceb2aa2eaf4ad
kentq6 <kentq6@gmail.com>
2025-12-13 00:32:43 -0500

    Rename SessionInfo to SessionDetails, rename header to reflect component name change, remove year from header to reduce redundancy, change flag border color

--- components/SessionDetails.tsx
+++ components/SessionDetails.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { useFilteredSession } from "@/app/providers/FilteredSessionProvider";
 
-const SessionInfo = () => {
+const SessionDetails = () => {
   const { filteredSession } = useFilteredSession();
 
   return (
     <div className="flex flex-col h-full">
-      <h1 className="text-sm font-bold pb-1">Session</h1>
+      <h1 className="text-sm font-bold pb-1">Session Details</h1>
       <Separator />
       {filteredSession ? (
         <div className="bg-background mt-2 rounded-sm sm:rounded-md border flex flex-col items-center justify-center h-full w-full gap-3">
           {/* Session Info */}
           <div className="flex flex-col items-center">
-            <div className="font-semibold text-lg">
-              {filteredSession.year} {filteredSession.country_name}
+            <div className="font-semibold text-lg text-center">
+              {filteredSession.country_name}
             </div>
-            <div className="text-sm lg:text-md opacity-80">
+            <div className="text-sm lg:text-md opacity-80 text-center">
               {new Date(filteredSession.date_start).toLocaleDateString(
                 "en-US",
                 {
@@ -31,7 +31,7 @@ const SessionInfo = () => {
 
           {/* Flag and Country Acronym */}
           <div className="flex flex-col items-center overflow-hidden">
-            <div className="w-24 h-16 md:w-48 md:h-32 lg:w-36 lg:h-24 rounded-md overflow-hidden border border-white">
+            <div className="w-24 h-16 md:w-48 md:h-32 lg:w-36 lg:h-24 rounded-md overflow-hidden border">
               <Image
                 src={`/country-flags/${filteredSession.country_code}.svg`}
                 width={100}
@@ -55,4 +55,4 @@ const SessionInfo = () => {
   );
 };
 
-export default SessionInfo;
+export default SessionDetails;
