46e2af7444a61a11b241022cad90d39d8620594c
kentq6 <kentq6@gmail.com>
2025-12-18 18:49:54 -0500

    Add redirect from root to dashboard in next.config and clean up HomePage component for best practices

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
