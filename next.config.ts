07678d1dadc3567cba4b12e025017f54dafb71c4
kentq6 <kentq6@gmail.com>
2025-12-23 19:58:09 -0500

    Update next.config.ts to standardize image URL patterns and add support for Clerk's CDN images

--- next.config.ts
+++ next.config.ts
@@ -4,25 +4,27 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'media.formula1.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "media.formula1.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.formula1.com',
-        pathname: '/content/dam/fom-website/drivers/**',
+        protocol: "https",
+        hostname: "www.formula1.com",
+        pathname: "/content/dam/fom-website/drivers/**",
       },
+      // Allows images from Clerk's CDN to be loaded as images
+      new URL("https://img.clerk.com/**"),
     ],
   },
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/dashboard',
+        source: "/",
+        destination: "/dashboard",
         permanent: true,
       },
-    ]
+    ];
   },
 };
 
