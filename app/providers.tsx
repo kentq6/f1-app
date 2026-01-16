5b8123c8cb6f14e65b3ae7926bae3026f95d33b4
kentq6 <kentq6@gmail.com>
2026-01-16 12:53:24 -0500

    Add Redux Toolkit and React-Redux for state management; implement StoreProvider for global state access

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,6 +10,7 @@ import { SessionsProvider } from "./providers/SessionsProvider";
 import QueryProvider from "./providers/QueryProvider";
 import { SessionInfoProvider } from "@/app/providers/SessionInfoProvider";
 import { SelectedDriversProvider } from "@/app/providers/SelectedDriversProvider";
+import { StoreProvider } from "@/store/storeProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -19,24 +20,26 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       // enableSystem
       disableTransitionOnChange
     >
-      <SessionFiltersProvider>
-        <FilteredSessionProvider>
-          <SessionsProvider>
-            <DriversProvider>
-              <QueryProvider>
-                <SessionInfoProvider>
-                  <SelectedDriversProvider>
-                    <SidebarProvider defaultOpen={false}>
-                      <AppSidebar />
-                      {children}
-                    </SidebarProvider>
-                  </SelectedDriversProvider>
-                </SessionInfoProvider>
-              </QueryProvider>
-            </DriversProvider>
-          </SessionsProvider>
-        </FilteredSessionProvider>
-      </SessionFiltersProvider>
+      <StoreProvider>
+        <SessionFiltersProvider>
+          <FilteredSessionProvider>
+            <SessionsProvider>
+              <DriversProvider>
+                <QueryProvider>
+                  <SessionInfoProvider>
+                    <SelectedDriversProvider>
+                      <SidebarProvider defaultOpen={false}>
+                        <AppSidebar />
+                        {children}
+                      </SidebarProvider>
+                    </SelectedDriversProvider>
+                  </SessionInfoProvider>
+                </QueryProvider>
+              </DriversProvider>
+            </SessionsProvider>
+          </FilteredSessionProvider>
+        </SessionFiltersProvider>
+      </StoreProvider>
     </ThemeProvider>
   );
 }
