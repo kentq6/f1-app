e44c8b1d030653e05fe4b8ffd7edc89efc32b3be
kentq6 <kentq6@gmail.com>
2026-01-18 19:27:21 -0500

    Update session and sessionResult types to improve structure and clarity; added optional points field and adjusted gap_to_leader type

--- types/session.ts
+++ types/session.ts
@@ -1,16 +1,16 @@
 export type Session = {
+  session_key: number | "latest";
+  session_type: string;
+  session_name: string;
+  date_start: string;
+  date_end: string;
+  meeting_key: number;
   circuit_key: number;
   circuit_short_name: string;
-  country_code: string;
   country_key: number;
+  country_code: string;
   country_name: string;
-  date_end: string;
-  date_start: string;
-  gmt_offset: string;
   location: string;
-  meeting_key: number;
-  session_key: number;
-  session_name: string;
-  session_type: string;
+  gmt_offset: string;
   year: number;
 };
--- types/sessionResult.ts
+++ types/sessionResult.ts
@@ -2,12 +2,12 @@ export type SessionResult = {
   position: number;
   driver_number: number;
   number_of_laps: number;
-  points: number;
+  points?: number; 
   dnf: boolean;
   dns: boolean;
   dsq: boolean;
-  gap_to_leader: number | string;
   duration: number;
+  gap_to_leader: number;
   meeting_key: number;
   session_key: number;
 };
